3bd20c8d991707a6c84108fefddad64ba5812519
vasu <vasucp1207@gmail.com>
2025-07-30 11:52:57 +0530

    remove gl4es configs

--- src/usr/lib/vicharak-config/tui/advanced/gpu/gpu.sh
+++ src/usr/lib/vicharak-config/tui/advanced/gpu/gpu.sh
@@ -18,26 +18,6 @@ __advanced_gpu_install() {
 	fi
 }
 
-__advanced_gpu_enable_opengl() {
-	if yesno "Are you sure to enable OpenGL support for Rockchip Mali gpu?"; then
-		if enable_gpu_opengl; then
-			msgbox "Enabled OpenGL support for Rockchip Mali gpu.\n\nUse 'gl4es' prefix to run OpenGL applications."
-		else
-			msgbox "Failed to enable OpenGL support for Rockchip Mali gpu."
-		fi
-	fi
-}
-
-__advanced_gpu_disable_opengl() {
-	if yesno "Are you sure to disable OpenGL support for Rockchip Mali gpu?"; then
-		if disable_gpu_opengl; then
-			msgbox "Disabled OpenGL support for Rockchip Mali gpu."
-		else
-			msgbox "Failed to disable OpenGL support for Rockchip Mali gpu."
-		fi
-	fi
-}
-
 __advanced_gpu() {
 	menu_init
 
@@ -45,12 +25,6 @@ __advanced_gpu() {
 		menu_add __advanced_gpu_uninstall "Uninstall Rockchip Mali gpu"
 	else
 		menu_add __advanced_gpu_install "Install Rockchip Mali gpu"
-	fi
-
-	if [ -f /usr/bin/gl4es ]; then
-		menu_add __advanced_gpu_disable_opengl "Disable OpenGL support for Rockchip Mali gpu"
-	else
-		menu_add __advanced_gpu_enable_opengl "Enable OpenGL support for Rockchip Mali gpu"
 	fi
 
 	menu_show "Please select an option below:"
